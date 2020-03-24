2e591ec7a126f6e479ea6ec89ba4e9142e0ba6b2
MatveyPower <55589736+MatveyPower@users.noreply.github.com>
2020-03-24 12:54:02 +0300

    add go to the questions page

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-let listOfQuestions = document.querySelector('.questions')
+let listOfQuestions = document.querySelector('.wrapper-questions')
+let windowToStartPolling = document.querySelector('.wrapper')
+let buttonToStartPolling = document.querySelector('.button-start')
+let buttonToConfirmAnswer = document.querySelector('.button-confirm-answer')
+
+
+listOfQuestions.classList.add('display-none')
+
+buttonToStartPolling.addEventListener('click', startPolling)
+
+function startPolling() {
+    windowToStartPolling.classList.add('display-none')
+    listOfQuestions.classList.remove('display-none')
+}
