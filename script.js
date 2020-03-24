f3f2868a34fad25a87fb235b65f2b56f09eafa93
MatveyPower <55589736+MatveyPower@users.noreply.github.com>
2020-03-24 16:16:53 +0300

    sending data to the server

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
+'use strict'
 let listOfQuestions = document.querySelector('.wrapper-questions')
 let windowToStartPolling = document.querySelector('.wrapper')
 let buttonToStartPolling = document.querySelector('.button-start')
 let buttonToConfirmAnswer = document.querySelector('.button-confirm-answer')
 
+let form = document.forms
+let questions = form.questions
+let question1Field = questions.elements.question1
+
+
+
 
 listOfQuestions.classList.add('display-none')
 
 buttonToStartPolling.addEventListener('click', startPolling)
+// buttonToConfirmAnswer.addEventListener('click', startPolling)
+buttonToConfirmAnswer.addEventListener('submit', submitForm)
+
 
 function startPolling() {
     windowToStartPolling.classList.add('display-none')
     listOfQuestions.classList.remove('display-none')
+}
+
+function submitForm(event){
+    event.preventDefault()
+
+    const api = 'https://httpbin.org/post'
+
+    let questions = {
+        question1: question1Field.value
+    }
+
+    fetch(api,{
+        headers:{
+            'Content-Type': 'application/json; charset=utf-8 '
+        },
+        method:'POST',
+        body: JSON.stringify(questions)
+    })
 }
