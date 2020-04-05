af24f0a14a8d016782a6912f8fb78e6f4c32ea17
MatveyPower <55589736+MatveyPower@users.noreply.github.com>
2020-04-05 20:03:50 +0300

    added localStorage

--- script.js
+++ script.js
@@ -12,32 +12,53 @@ surveyResultPage.classList.add('display-none')
 
 buttonToStartSurvey.addEventListener('click', startSurvey)
 buttonToSendAnswers.addEventListener('click', sendAnswers)
-window.addEventListener('load', getAnswerValues)
-
-
+window.addEventListener('load', activateLocalStorage)
 
 function startSurvey() {
   surveyPreviewPage.classList.add('display-none')
   questionsPage.classList.remove('display-none')
 }
 
-function getAnswerValues(){
-    answerFields.forEach(answer => {
-        answer.value = localStorage.getItem(`${answer.id}`)
-        answer.addEventListener('input', setAnswerValues, answer)
-    })
+function activateLocalStorage() {
+  if (localStorage.length) {
+    buttonToStartSurvey.textContent = 'Продолжить'
+  }
+
+  answerFields.forEach((answer) => {
+    answer.value = localStorage.getItem(`${answer.id}`)
+    answer.addEventListener('input', setAnswerValues)
+  })
 }
 
-function setAnswerValues(answer){
-    localStorage.setItem(answer.id,answer.value)
+function setAnswerValues(event) {
+  const answer = event.target
+
+  localStorage.setItem(answer.id, answer.value)
 }
 
 function sendAnswers(event) {
+  const warningTextParagraph = document.querySelector(
+    '.questions__warning-text'
+  )
   event.preventDefault()
+
+  warningTextParagraph.classList.add('display-none')
+  buttonToSendAnswers.disabled = true
+  buttonToSendAnswers.classList.add('questions__button--disabled')
+  buttonToSendAnswers.firstElementChild.classList.add('display-none')
+  buttonToSendAnswers.lastElementChild.classList.remove('display-none')
 
   const url = 'https://api.teamunite.ru/api/v1/questions'
 
   const answers = answerFields.map((field) => field.value)
+
+  function handleError() {
+    buttonToSendAnswers.disabled = false
+    buttonToSendAnswers.classList.remove('questions__button--disabled')
+    buttonToSendAnswers.firstElementChild.classList.remove('display-none')
+    buttonToSendAnswers.lastElementChild.classList.add('display-none')
+    warningTextParagraph.classList.remove('display-none')
+  }
 
   fetch(url, {
     headers: {
@@ -46,7 +67,16 @@ function sendAnswers(event) {
     method: 'POST',
     body: JSON.stringify(answers),
   })
-
-  questionsPage.classList.add('display-none')
-  surveyResultPage.classList.remove('display-none')
+    .then((response) => {
+      if (response.ok) {
+        localStorage.clear()
+        questionsPage.classList.add('display-none')
+        surveyResultPage.classList.remove('display-none')
+      } else {
+        handleError()
+      }
+    })
+    .catch((error) => {
+      handleError()
+    })
 }
