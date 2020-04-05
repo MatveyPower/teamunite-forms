825a9168139cb455fcac1927fe1b916d6f03cf3f
MatveyPower <55589736+MatveyPower@users.noreply.github.com>
2020-04-05 18:12:28 +0300

    rewrite code with БЭМ

--- script.js
+++ script.js
@@ -1,70 +1,38 @@
 'use strict'
-document.cookie = 'expires=Tue, 19 Jan 2038 03:14:07 GMT'
-console.log(document.cookie)
-let listOfQuestions = document.querySelector('.wrapper-questions')
-let windowToStartPolling = document.querySelector('.wrapper')
-let buttonToStartPolling = document.querySelector('.button-start')
-let buttonToConfirmAnswer = document.querySelector('.button-confirm-answer')
-let windowToEndPolling = document.querySelector('.wrapper-end')
 
-let forms = document.forms
-let currentForm = forms.questions
-let question1Field = questions.elements.question1
-let question2Field = questions.elements.question2
-let question3Field = questions.elements.question3
-let question4Field = questions.elements.question4
-let question5Field = questions.elements.question5
-let question6Field = questions.elements.question6
-let question7Field = questions.elements.question7
-let question8Field = questions.elements.question8
-let question9Field = questions.elements.question9
-let question10Field = questions.elements.question10
-let question11Field = questions.elements.question11
+const questionsPage = document.querySelector('.questions')
+const surveyPreviewPage = document.querySelector('.survey-preview')
+const buttonToStartSurvey = document.querySelector('.survey-preview__button')
+const buttonToSendAnswers = document.querySelector('.questions__button')
+const surveyResultPage = document.querySelector('.survey-result')
+const answerFields = [...document.querySelectorAll('.question__answer-text')]
 
+questionsPage.classList.add('display-none')
+surveyResultPage.classList.add('display-none')
 
+buttonToStartSurvey.addEventListener('click', startSurvey)
+buttonToSendAnswers.addEventListener('click', sendAnswers)
 
-
-listOfQuestions.classList.add('display-none')
-windowToEndPolling.classList.add('display-none')
-
-buttonToStartPolling.addEventListener('click', startPolling)
-// buttonToConfirmAnswer.addEventListener('click', startPolling)
-currentForm.addEventListener('submit', submitForm)
-
-
-function startPolling() {
-    windowToStartPolling.classList.add('display-none')
-    listOfQuestions.classList.remove('display-none')
+function startSurvey() {
+  surveyPreviewPage.classList.add('display-none')
+  questionsPage.classList.remove('display-none')
 }
 
-function submitForm(event){
-    event.preventDefault()
+function sendAnswers(event) {
+  event.preventDefault()
 
-    const api = 'http://teamunite-backend.herokuapp.com/api/v1/questions'
+  const url = 'https://api.teamunite.ru/api/v1/questions'
 
-    let questions = {
-        question1: question1Field.value,
-        question2: question2Field.value,
-        question3: question3Field.value,
-        question4: question4Field.value,
-        question5: question5Field.value,
-        question6: question6Field.value,
-        question7: question7Field.value,
-        question8: question8Field.value,
-        question9: question9Field.value,
-        question10: question10Field.value,
-        question11: question11Field.value,
-    }
+  const answers = answerFields.map((field) => field.value)
 
-    fetch(api,{
-        headers:{
-            'Content-Type': 'application/json; charset=utf-8'
-        },
-        method:'POST',
-        body: JSON.stringify(questions)
-    })
+  fetch(url, {
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+    method: 'POST',
+    body: JSON.stringify(answers),
+  })
 
-    listOfQuestions.classList.add('display-none')
-    windowToEndPolling.classList.remove('display-none')
-
+  questionsPage.classList.add('display-none')
+  surveyResultPage.classList.remove('display-none')
 }
